03c10afb634654d04cd9523bb88698b9aec052c8
Commissioner <redregion@mail.ru>
2021-10-19 20:46:48 +0500

    Add project files.

---
+++ SLAE_Resolver_Gauss/Gauss.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SLAE_Resolver_Gauss
+{
+    static class Gauss
+    {
+        static void ChangeRows(double[,] m, double[] fv, int i1, int i2)
+        {
+            double rv;
+            for (int j = 0; j < m.GetLength(1); j++) 
+            { 
+                rv = m[i1, j];
+                m[i1, j] = m[i2, j]; 
+                m[i2, j] = rv; 
+            }
+            rv = fv[i1]; 
+            fv[i1] = fv[i2]; 
+            fv[i2] = rv; 
+        }
+
+        static void SubtractionMtpRow(double[,] m, double[] fv, int i1, int i2, double mtp)
+        {
+            for (int j = 0; j < m.GetLength(1); j++)
+            {
+                m[i1, j] -= m[i2, j] * mtp;
+            }
+            fv[i1] -= fv[i2] * mtp;
+        }
+
+        static void MatrixToRightUpperTriangularMatrix(double[,] m, double[] fv)
+        {
+            int d = m.GetLength(0);
+            for (int j = 0; j < d - 1; j++)
+            {
+                int ir = j;
+                for (int i = j; i < d; i++)
+                {
+                    if (Math.Abs(m[i, j]) > Math.Abs(m[ir, j]))
+                    {
+                        ir = i;
+                    }
+                }
+                if (ir != j) 
+                { 
+                    ChangeRows(m, fv, ir, j); 
+                }
+                for (int i = d - 1; i > j; i--)
+                {
+                    SubtractionMtpRow(m, fv, i, j, m[i, j] / m[j, j]);
+                }
+            }
+        }
+
+        public static double[] Solve(double[,] em, double[] fv)
+        {
+            int d = em.GetLength(0);
+            double[] res = new double[d];
+            MatrixToRightUpperTriangularMatrix(em, fv);
+            for (int i = d - 1; i >= 0; i--)
+            {
+                for (int j = d - 1; j > i; j--)
+                {
+                    fv[i] -= em[i, j] * fv[j];
+                }
+                fv[i] = fv[i] / em[i, i];
+            }
+            return fv;
+        }
+    }
+}
---
+++ SLAE_Resolver_Gauss/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace SLAE_Resolver_Gauss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter SLAE.txt full name:");
+            var filePath = Console.ReadLine();
+            var lines = File.ReadAllLines(filePath);
+
+            var n = lines.Length;
+            var m = new double[n, n];
+            var v = new double[n];
+
+            for(int i = 0; i < n; i++)
+            {
+                var cells = lines[i].Split('|');
+                for (int j = 0; j < n; j++)
+                {
+                    m[i, j] = double.Parse(cells[j]);
+                }
+                v[i] = double.Parse(cells[lines.Length]);
+            }
+
+            var t = DateTime.Now;
+            var result = Gauss.Solve(m, v);
+            var ts = DateTime.Now - t;
+
+            for(int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine($"x[{i}] = {Math.Round(result[i])}");
+            }
+            Console.WriteLine($"Solved in {ts}");
+        }
+    }
+}
